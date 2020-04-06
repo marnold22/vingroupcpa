d96f2db675031e803d1ba03f5f551c6d1c784bc2
Michael <minimearnold@gmail.com>
2020-04-06 08:25:19 -0700

    Added navbar back to each page that way active state displays correctly

--- index.php
+++ index.php
@@ -7,7 +7,38 @@
 <body>
 
   <!-- Navigation -->
-  <?php include("modules/navbar.php") ?>
+  <nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark shadow fixed-top bg-cpagreen">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+        <img src="assets/logo.jpg" width="30" height="30" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="services.php">Services</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="events.php">Events</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="about.php">About</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://voffice.dillners.com/securelogin.asp?acct=23627" target="_blank">Client Login</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+</nav>
 
   <!-- Hero Banner -->
   <header class="herobanner herobanner-home">
