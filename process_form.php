b51bc6a009b92aa2040efaf2ac7fb12f35523b6a
Michael <minimearnold@gmail.com>
2020-04-10 12:05:52 -0700

    Moved process_form.php to root from modules that way it can access .env file

--- process_form.php
+++ process_form.php
@@ -1,5 +1,4 @@
 <?php
-
     // Import PHPMailer classes into the global namespace
     // These must be at the top of your script, not inside a function
     use PHPMailer\PHPMailer\PHPMailer;
@@ -9,6 +8,9 @@
     // Load Composer's autoloader
     require 'vendor/autoload.php';
 
+    //Load .env file
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
 
     // Define variables and set to empty
     $fname = $lname = $email = $checklist = $business = $message = "";
@@ -92,14 +94,14 @@
             $mail->isSMTP();                                            // Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'USERNAME';                     // SMTP username
-            $mail->Password   = 'PASSWORD';                               // SMTP password
+            $mail->Username   = $_ENV['FROM_EMAIL'];                     // SMTP username
+            $mail->Password   = $_ENV['FROM_EMAIL_PASS'];                               // SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
             $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
             //Recipients
             $mail->setFrom('vingroupcpa@gmail.com', 'Vingroupcpa Website');
-            $mail->addAddress('TO_EMAIL', 'Vingroupcpa');     // Add a recipient
+            $mail->addAddress($_ENV['TO_EMAIL'], 'Vingroupcpa');     // Add a recipient
 
             // Content
             $mail->isHTML(true);                                  // Set email format to HTML
