d35233c94deb75b51ae711d917095c38db27272a
Michael <michaelarnold615@gmail.com>
2020-06-04 10:07:11 -0700

    Updated Covid-19 Banner Response

--- modules/covid19-modal.php
+++ modules/covid19-modal.php
@@ -9,8 +9,11 @@
           </div>
           <div class="modal-body">
             <div class="row">
-              <div class="text-center">
-                <p id="covid-19">Under the current Washington State regulations we are qualified as an “Essential Service” to continue to staff our office. We will continue doing payroll, bookkeeping and tax preparation as usual. In order to keep our clients safe and healthy, as well as our staff, some of our procedures will be a little different. Even though our doors are locked there are several ways that you can get information to us and for us to get information to you. You can mail, email, fax, access the portal, call, face time and/or drop off paperwork in the back mail slot of our building. We are here working and available.</p>
+              <div class="text-center covid-modal">
+                <p id="covid-19"><strong>Now that we are in the next Phase under the current Washington State regulations the office is now open and appointments are available.</strong></p>
+                <p id="covid-19">In order to keep our clients safe and healthy, as well as our staff, some of our procedures will be a little different. We will be allowing a space of time between appointments to give time to wipe down areas, pens, etc.. We will also be cleaning countertops, chairs, work spaces, door handles, etc. periodically through the day. Even though appointments are available, please feel free to access us through any of the following ways: mail, email, fax, access the portal, call, face time and/or drop off paperwork in person or in the back mail slot of our building.</p>
+                <p id="covid-19"><strong>We are here working and available. <br> 590-443-4262 or cpa@vingroup.com</strong></p>
+                <br>
                 <a class="btn-c19" href="../assets/pdf/CARES_Act_Visual.pdf" target="_blank" role="button" >Cares Act Visual</a>
                 <a class="btn-c19" href="../assets/pdf/covid19sbassistanceprgms.pdf" target="_blank" role="button" >Covid-19 Assistance</a>
               </div>
