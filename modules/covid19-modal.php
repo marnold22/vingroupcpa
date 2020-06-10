59d51f8d212d28a690850b2a445ce1989e20b37d
Michael <michaelarnold615@gmail.com>
2020-06-09 17:38:20 -0700

    Covid-19 Modal Update
    Removed the buttons from modal as they were for Phase 1

--- modules/covid19-modal.php
+++ modules/covid19-modal.php
@@ -13,9 +13,9 @@
                 <p id="covid-19"><strong>Now that we are in the next Phase under the current Washington State regulations the office is now open and appointments are available.</strong></p>
                 <p id="covid-19">In order to keep our clients safe and healthy, as well as our staff, some of our procedures will be a little different. We will be allowing a space of time between appointments to give time to wipe down areas, pens, etc.. We will also be cleaning countertops, chairs, work spaces, door handles, etc. periodically through the day. Even though appointments are available, please feel free to access us through any of the following ways: mail, email, fax, access the portal, call, face time and/or drop off paperwork in person or in the back mail slot of our building.</p>
                 <p id="covid-19"><strong>We are here working and available. <br> 590-443-4262 or cpa@vingroup.com</strong></p>
-                <br>
-                <a class="btn-c19" href="../assets/pdf/CARES_Act_Visual.pdf" target="_blank" role="button" >Cares Act Visual</a>
-                <a class="btn-c19" href="../assets/pdf/covid19sbassistanceprgms.pdf" target="_blank" role="button" >Covid-19 Assistance</a>
+                <!-- Removed Covid-19 Buttons From Modal -->
+                <!-- <a class="btn-c19" href="../assets/pdf/CARES_Act_Visual.pdf" target="_blank" role="button" >Cares Act Visual</a> -->
+                <!-- <a class="btn-c19" href="../assets/pdf/covid19sbassistanceprgms.pdf" target="_blank" role="button" >Covid-19 Assistance</a> -->
               </div>
             </div>
           </div>
