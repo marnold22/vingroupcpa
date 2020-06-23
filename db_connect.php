b32b1c670dd40f1826358938db2e1528e2da28e5
Michael <minimearnold@gmail.com>
2020-06-23 14:07:16 -0700

    Establish database connection
    This pulls database credentials from the ENV file

---
+++ db_connect.php
@@ -0,0 +1,22 @@
+<?php
+
+    // Load Composer's autoloader
+    require 'vendor/autoload.php';
+    //Load .env file
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $db_server = $_ENV['DB_SERVER'];
+    $db_username = $_ENV['DB_USERNAME'];
+    $db_password = $_ENV['DB_PASSWORD'];
+    $db_name = $_ENV['DB_NAME'];
+
+    /* Attempt to connect to MySQL database */
+    $connect = mysqli_connect($db_server, $db_username, $db_password, $db_name);
+
+    // Check connection
+    if(!$connect){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+
+?>
