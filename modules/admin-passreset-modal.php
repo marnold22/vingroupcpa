79f1e9f2876ec12d6808a820ae26c39bf0f92e5b
Michael <minimearnold@gmail.com>
2020-06-29 18:46:49 -0700

    Created admin password reset modal

---
+++ modules/admin-passreset-modal.php
@@ -0,0 +1,39 @@
+<div class="modal fade" id="adminPassResetModal" tabindex="-1" role="dialog" aria-labelledby="adminModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header modal-hf">
+        <h5 class="modal-title" id="modalTitle">Admin Reset Password</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
+            <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+                <label>New Password</label>
+                <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
+                <span class="help-block"><?php echo $new_password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <a class="btn-services" href="admin.php">Cancel</a>
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer modal-hf">
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Prevent resubmissoin of form on page refresh -->
+<script>
+  if (window.history.replaceState) {
+    window.history.replaceState(null, null, window.location.href);
+  }
+</script>
