956d5c2f1d5e337bd0ca47f86c2ca09ebb31d812
Michael <minimearnold@gmail.com>
2020-06-29 18:49:32 -0700

    Updated password reset to same styles

--- admin_reset_password.php
+++ admin_reset_password.php
@@ -72,35 +72,63 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
  
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Reset Password</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
+
+<!-- Head -->
+<?php include("modules/head.php") ?>
+
+<!-- Navigation -->
+<nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark shadow fixed-top bg-cpagreen">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/logo.jpg" width="30" height="30" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin.php">Admin Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_logout.php">Logout</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <body>
-    <div class="wrapper">
-        <h2>Reset Password</h2>
-        <p>Please fill out this form to reset your password.</p>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post"> 
-            <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
-                <label>New Password</label>
-                <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
-                <span class="help-block"><?php echo $new_password_err; ?></span>
+    <!-- Hero Banner -->
+    <header class="herobanner herobanner-admin-passreset">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12 text-center">
+                    <h1>Reset Password</h1>
+                </div>
             </div>
-            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                <label>Confirm Password</label>
-                <input type="password" name="confirm_password" class="form-control">
-                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+        </div>
+        <!-- Modal -->
+        <?php include("modules/admin-passreset-modal.php") ?>
+    </header>
+
+    <!-- Footer -->
+    <footer class="py-3">
+        <div class="container">
+            <div class="row">
             </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link" href="admin.php">Cancel</a>
-            </div>
-        </form>
-    </div>    
+        </div>
+    </footer>
+
+    <!-- Scripts -->
+    <?php include("modules/scripts.php") ?>
+
+
+    <script type="text/javascript">
+        $(window).on('load', function() {
+            $('#adminPassResetModal').modal('show');
+        });
+    </script>
 </body>
+
 </html>
