848f6a5d6cd628353fa61a80408e372a4d76ee26
Michael <minimearnold@gmail.com>
2020-06-29 18:50:07 -0700

    Upsated navbar and admin landing page

--- admin.php
+++ admin.php
@@ -1,31 +1,65 @@
 <?php
 // Initialize the session
 session_start();
- 
+
 // Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     header("location: admin_login.php");
     exit;
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px sans-serif; text-align: center; }
-    </style>
-</head>
-<body>
-    <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+
+<!-- Head -->
+<?php include("modules/head.php") ?>
+
+<!-- Navigation -->
+<nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark shadow fixed-top bg-cpagreen">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">
+            <img src="assets/logo.jpg" width="30" height="30" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_logout.php">Logout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_reset_password.php">Reset Password</a>
+                </li>
+            </ul>
+        </div>
     </div>
-    <p>
-        <a href="admin_reset_password.php" class="btn btn-warning">Reset Your Password</a>
-        <a href="admin_logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
+</nav>
+
+<body>
+    <!-- Hero Banner -->
+    <header class="herobanner herobanner-admin-home">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12 text-center">
+                    <h1>Welcome</h1>
+                    <h1><?php echo htmlspecialchars($_SESSION["username"]); ?></h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Footer -->
+    <footer class="py-3">
+        <div class="container">
+            <div class="row">
+            </div>
+        </div>
+    </footer>
+
+    <!-- Scripts -->
+    <?php include("modules/scripts.php") ?>
 </body>
+
 </html>
