d7fbad68035bb9f09682cd9728567d6e992e4d68
Michael <michaelarnold615@gmail.com>
2020-07-21 12:37:29 -0700

    Testing file uploader

--- _news_add.php
+++ _news_add.php
@@ -7,6 +7,13 @@
     $title = $content = $image = "";
     $title_err = $content_err = $image_err = "";
 
+
+
+    $target_dir = "assets/news/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
     //To SANITIZE String value use
     function StringInputCleaner($data)
     {
@@ -35,9 +42,35 @@
         if (empty(trim($_POST["cimage"]))) {
             $image = "assets/news/news-default.jpg";
         } else {
-            // Need to check to make sure only a picture file is uploaded
-            // $image = trim($_POST["cmedia"]);
-            $image = "assets/news/news-default.jpg";
+
+            // Check File Type
+            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg") {
+                echo "Sorry, only JPG, JPEG, & PNG files are allowed.";
+                $uploadOk = 0;
+            }else {
+                // Check File Size
+                if ($_FILES["fileToUpload"]["size"] > 500000) {
+                    echo "Sorry, your file is too large.";
+                    $uploadOk = 0;
+                } else {
+                    $uploadOk = 1;
+                }
+            }
+
+            // Check if $uploadOk is set to 0 by an error
+            if ($uploadOk == 0) {
+                echo "Sorry, your file was not uploaded.";
+            // if everything is ok, try to upload file
+            } else {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                $file = $target_dir . basename( $_FILES["fileToUpload"]["name"]);
+                } else {
+                echo "Sorry, there was an error uploading your file.";
+                }
+            }
+
+        
         }
 
         // Validate Content
@@ -65,5 +98,3 @@
             mysqli_close($connect);            
         }
     }
-
-?>
