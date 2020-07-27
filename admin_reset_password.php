e00905c94a6c0dc13259b0b1a5085d0acd946d64
Michael <michaelarnold615@gmail.com>
2020-07-27 12:05:48 -0700

    Applied dashboard template to users page and reset password page

--- admin_reset_password.php
+++ admin_reset_password.php
@@ -62,81 +62,68 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     mysqli_close($connect);
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
-
 <!-- Head -->
 <?php include("modules/admin_head.php") ?>
 
-<!-- Navigation -->
-<nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark shadow fixed-top bg-cpagreen">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">
-            <img src="assets/logo.jpg" width="30" height="30" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="admin.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_news.php">News</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_users.php">Users</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="admin_reset_password.php">Password Reset</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_logout.php">Logout</a>
-                </li>
-            </ul>
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="admin.php">Vingroupcpa Admin</a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Admin</div>
+                        <a class="nav-link" href="admin.php" aria-expanded="false">Home</a>
+                        <a class="nav-link" href="admin_news.php" aria-expanded="false">News Posts</a>
+                        <a class="nav-link" href="admin_users.php" aria-expanded="false">Users Table</a>
+                        <a class="nav-link" href="admin_reset_password.php" aria-expanded="false">Password Resset</a>
+                        <a class="nav-link" href="#" aria-expanded="false">Register</a>
+                        <a class="nav-link" href="admin_logout.php" aria-expanded="false">Logout</a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    <?php echo htmlspecialchars($_SESSION["username"]); ?>
+                </div>
+            </nav>
         </div>
-    </div>
-</nav>
+        <div id="layoutSidenav_content">
+            <!-- PAGE UNIQUE CONTET GOES HERE -->
+            <!-- Password Reset Form -->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                        <div class="card card-signin">
+                            <div class="card-body">
+                                <h5 class="card-title text-center">Password Reset</h5>
+                                <form class="form-signin" action="" method="POST">
 
-<body>
-    <!-- Password Reset Form -->
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-                <div class="card card-signin">
-                    <div class="card-body">
-                        <h5 class="card-title text-center">Password Reset</h5>
-                        <form class="form-signin" action="" method="POST">
+                                    <div class="form-label-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+                                        <input name="new_password" type="text" id="newPassword" class="form-control" placeholder="New Password" required autofocus>
+                                        <label for="newPassword">New Password</label>
+                                        <span class="help-block"><?php echo $new_password_err; ?></span>
+                                    </div>
 
-                            <div class="form-label-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
-                                <input name="new_password" type="text" id="newPassword" class="form-control" placeholder="New Password" required autofocus>
-                                <label for="newPassword">New Password</label>
-                                <span class="help-block"><?php echo $new_password_err; ?></span>
+                                    <div class="form-label-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                                        <input name="confirm_password" type="text" id="confirmPassword" class="form-control" placeholder="Confirm Password" required>
+                                        <label for="confirmPassword">Confirm Password</label>
+                                        <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                                    </div>
+                                    <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Confirm</button>
+
+                                </form>
                             </div>
-
-                            <div class="form-label-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
-                                <input name="confirm_password" type="text" id="confirmPassword" class="form-control" placeholder="Confirm Password" required>
-                                <label for="confirmPassword">Confirm Password</label>
-                                <span class="help-block"><?php echo $confirm_password_err; ?></span>
-                            </div>
-                            <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Confirm</button>
-
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
-    <!-- Footer -->
-    <footer class="py-3">
-        <div class="container">
-            <div class="row">
-            </div>
-        </div>
-    </footer>
 
     <!-- Scripts -->
     <?php include("modules/scripts.php") ?>
--- admin_users.php
+++ admin_users.php
@@ -1,122 +1,98 @@
 <?php include("_admin_check_loggedout.php") ?>
-
-<?php
-    // Include config file
-    require_once 'db_connect.php';
-?>
-
-
+<?php require_once 'db_connect.php'; ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
-
 <!-- Head -->
 <?php include("modules/admin_head.php") ?>
 
-<!-- Navigation -->
-<nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark shadow fixed-top bg-cpagreen">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">
-            <img src="assets/logo.jpg" width="30" height="30" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="admin.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_news.php">News</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="admin_users.php">Users</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_reset_password.php">Password Reset</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_logout.php">Logout</a>
-                </li>
-            </ul>
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="admin.php">Vingroupcpa Admin</a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Admin</div>
+                        <a class="nav-link" href="admin.php" aria-expanded="false">Home</a>
+                        <a class="nav-link" href="admin_news.php" aria-expanded="false">News Posts</a>
+                        <a class="nav-link" href="admin_users.php" aria-expanded="false">Users Table</a>
+                        <a class="nav-link" href="admin_reset_password.php" aria-expanded="false">Password Resset</a>
+                        <a class="nav-link" href="#" aria-expanded="false">Register</a>
+                        <a class="nav-link" href="admin_logout.php" aria-expanded="false">Logout</a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    <?php echo htmlspecialchars($_SESSION["username"]); ?>
+                </div>
+            </nav>
         </div>
-    </div>
-</nav>
-
-<body>
-    <!-- News Table -->
-    <div id="adminusers-page" class="admin-users">
-        <div class="container-xl">
-            <div class="table-responsive">
-                <div class="table-wrapper">
-                    <div class="table-title">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <h2>Manage <b>Users</b></h2>
+        <div id="layoutSidenav_content">
+            <!-- PAGE UNIQUE CONTET GOES HERE -->
+            <!-- News Table -->
+            <div id="adminusers-page" class="admin-users">
+                <div class="container-xl">
+                    <div class="table-responsive">
+                        <div class="table-wrapper">
+                            <div class="table-title">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <h2>Manage <b>Users</b></h2>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <!-- <a href="#addNewsModal" class="btn btn-success" data-toggle="modal"><i class="fa fa-plus" aria-hidden="true"></i><span> Add News Post</span></a> -->
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-sm-6">
-                                <!-- <a href="#addNewsModal" class="btn btn-success" data-toggle="modal"><i class="fa fa-plus" aria-hidden="true"></i><span> Add News Post</span></a> -->
-                            </div>
+                            <table class="table table-striped table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Username</th>
+                                        <th>TOOLS</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- START LOOP -->
+                                    <?php
+                                    // Attempt select query execution
+                                    $sql = "SELECT * FROM users";
+                                    if ($result = mysqli_query($connect, $sql)) {
+                                        if (mysqli_num_rows($result) > 0) {
+                                            while ($row = mysqli_fetch_array($result)) {
+                                                echo "<tr>";
+                                                echo "<td class='overflow'>" . $row['id'] . "</td>";
+                                                echo "<td class='overflow'>" . $row['username'] . "</td>";
+                                                echo "<td class='overflow'></td>";
+                                                echo "</tr>";
+                                            }
+                                            // Free result set
+                                            mysqli_free_result($result);
+                                        } else {
+                                            echo "No records matching your query were found.";
+                                        }
+                                    } else {
+                                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                                    }
+                                    ?>
+                                    <!-- END LOOP -->
+                                </tbody>
+                            </table>
                         </div>
                     </div>
-                    <table class="table table-striped table-hover">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Username</th>
-                                <th>TOOLS</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <!-- START LOOP -->
-                            <?php
-                            // Attempt select query execution
-                            $sql = "SELECT * FROM users";
-                            if ($result = mysqli_query($connect, $sql)) {
-                                if (mysqli_num_rows($result) > 0) {
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        echo "<tr>";
-                                        echo "<td class='overflow'>" . $row['id'] . "</td>";
-                                        echo "<td class='overflow'>" . $row['username'] . "</td>";
-                                        echo "<td class='overflow'></td>";
-                                        echo "</tr>";
-                                    }
-                                    // Free result set
-                                    mysqli_free_result($result);
-                                } else {
-                                    echo "No records matching your query were found.";
-                                }
-                            } else {
-                                echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-                            }
-                            ?>
-                            <!-- END LOOP -->
-                        </tbody>
-                    </table>
                 </div>
             </div>
+            <!-- End of Table -->
         </div>
     </div>
-    <!-- End of Table -->
-
-
-    <!-- ******************** START ALL MODALS ******************** -->
-
-    <!-- ******************** END ALL MODALS ******************** -->
-
-    
-    <!-- Footer -->
-    <footer class="py-3">
-        <div class="container">
-            <div class="row"></div>
-        </div>
-    </footer>
 
     <!-- Scripts -->
     <?php include("modules/scripts.php") ?>
-    <!-- Prevent resubmissoin of form on page refresh -->
     <script>
         if (window.history.replaceState) {
             window.history.replaceState(null, null, window.location.href);
