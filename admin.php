36e3168424a3391e3562810de7ff64d8b2f3b042
Michael <michaelarnold615@gmail.com>
2020-07-27 11:55:06 -0700

    Revamped admin home page using dashboard style

--- admin.php
+++ admin.php
@@ -2,64 +2,38 @@
 
 <!DOCTYPE html>
 <html lang="en">
-
 <!-- Head -->
 <?php include("modules/admin_head.php") ?>
-
-<!-- Navigation -->
-<nav id="main-navbar" class="navbar navbar-expand-lg navbar-dark shadow fixed-top bg-cpagreen">
-    <div class="container">
-        <a class="navbar-brand" href="index.php">
-            <img src="assets/logo.jpg" width="30" height="30" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link active" href="admin.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_news.php">News</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_users.php">Users</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_reset_password.php">Password Reset</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="admin_logout.php">Logout</a>
-                </li>
-            </ul>
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <a class="navbar-brand" href="admin.php">Vingroupcpa Admin</a>
+        <button class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Admin</div>
+                        <a class="nav-link" href="#" aria-expanded="false">Home</a>
+                        <a class="nav-link" href="admin_news.php" aria-expanded="false">News Posts</a>
+                        <a class="nav-link" href="admin_users.php" aria-expanded="false">Users Table</a>
+                        <a class="nav-link" href="admin_reset_password.php" aria-expanded="false">Password Resset</a>
+                        <a class="nav-link" href="#" aria-expanded="false">Register</a>
+                        <a class="nav-link" href="admin_logout.php" aria-expanded="false">Logout</a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    <?php echo htmlspecialchars($_SESSION["username"]); ?>
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
         </div>
     </div>
-</nav>
-
-<body>
-    <!-- Hero Banner -->
-    <header class="herobanner herobanner-admin-home">
-        <div class="container h-100">
-            <div class="row h-100 align-items-center">
-                <div class="col-12 text-center">
-                    <h1>Welcome</h1>
-                    <h1><?php echo htmlspecialchars($_SESSION["username"]); ?></h1>
-                </div>
-            </div>
-        </div>
-    </header>
-
-    <!-- Footer -->
-    <footer class="py-3">
-        <div class="container">
-            <div class="row">
-            </div>
-        </div>
-    </footer>
 
     <!-- Scripts -->
     <?php include("modules/scripts.php") ?>
 </body>
-
 </html>
