8a44e4bf4e9402cf01bade07916d62503e86376d
Michael <michaelarnold615@gmail.com>
2020-07-27 11:54:33 -0700

    Admin dashboard overhaul
    Added admin-styles.css & admin.js to the admin header and scripts files

--- modules/scripts.php
+++ modules/scripts.php
@@ -8,3 +8,4 @@
 <script src="js/popper.min.js" charset="utf-8"></script>
 <script src="js/bootstrap.min.js" charset="utf-8"></script>
 <script src="js/main.js" charset="utf-8"></script>
+<script src="js/admin.js" charset="utf-8"></script>
