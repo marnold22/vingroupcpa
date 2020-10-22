745f3c5483d484d78e016a0c9ff4c5058033fbf5
Michael <michaelarnold615@gmail.com>
2020-10-22 10:55:30 -0700

    Covid-19 Banner Update
    Updated the modal popup on the index page with the new covid-19 response. Modeled after the document sent.

--- modules/covid19-modal.php
+++ modules/covid19-modal.php
@@ -10,9 +10,11 @@
           <div class="modal-body">
             <div class="row">
               <div class="text-center covid-modal">
-                <p id="covid-19"><strong>Now that we are in the next Phase under the current Washington State regulations the office is now open and appointments are available.</strong></p>
-                <p id="covid-19">In order to keep our clients safe and healthy, as well as our staff, some of our procedures will be a little different. We will be allowing a space of time between appointments to give time to wipe down areas, pens, etc.. We will also be cleaning countertops, chairs, work spaces, door handles, etc. periodically through the day. Even though appointments are available, please feel free to access us through any of the following ways: mail, email, fax, access the portal, call, face time and/or drop off paperwork in person or in the back mail slot of our building.</p>
-                <p id="covid-19"><strong>We are here working and available. <br> 590-443-4262 or cpa@vingroup.com</strong></p>
+                <p id="covid-19-title"><strong>WE ARE HERE WORKING AND AVAILABLE</strong></p>
+                <p id="covid-19"><strong>Under the current Washington State regulations our office is open but you will need to call for an appointment.</strong></p>
+                <p id="covid-19">In order to keep our clients safe and healthy, as well as our staff, some of our procedures will be a little different. We will be allowing a space of time between appointments to give time to wipe down areas, pens, etc.. We will also be cleaning countertops, chairs, work spaces, door handles, etc. periodically through the day.</p>
+                <p id="covid-19"><i>Even though appointments are available, please feel free to access us through any of the following ways: mail, email, fax, access the portal, call, face time and/or drop off paperwork in person or in the back mail slot of our building.</i></p>
+                <p id="covid-19"><strong>590-443-4262 or cpa@vingroup.com</strong></p>
                 <!-- Removed Covid-19 Buttons From Modal -->
                 <!-- <a class="btn-c19" href="../assets/pdf/CARES_Act_Visual.pdf" target="_blank" role="button" >Cares Act Visual</a> -->
                 <!-- <a class="btn-c19" href="../assets/pdf/covid19sbassistanceprgms.pdf" target="_blank" role="button" >Covid-19 Assistance</a> -->
