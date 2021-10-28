a6a7627f4d240a0e68a0e36011282a6e2786ffcd
Michael <michaelarnold615@gmail.com>
2021-10-28 09:56:23 -0700

    Removed old commented code from scripts, header, footer
    This removes all old commented code from:
    scripts.php
    admin_head.php
    head.php
    
    These comments were links to CSS files (locally and CDN)

--- modules/scripts.php
+++ modules/scripts.php
@@ -1,15 +1,6 @@
-<!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script> -->
-
-
 <!-- Linked Bootstrap jQuery & popper locally rather than CDN for speed testing -->
 <script src="js/jquery-3.4.1.min.js" charset="utf-8"></script>
 <script src="js/popper.min.js" charset="utf-8"></script>
 <script src="js/bootstrap.min.js" charset="utf-8"></script>
 <script src="js/main.js" charset="utf-8"></script>
 <script src="js/admin.js" charset="utf-8"></script>
-
-<!-- Temporarily disabled these js files to call them only on the heic-cnvert page -->
-<!-- <script src="js/convert.js" charset="utf-8"></script>
-<script src="js/libheif.js" charset="utf-8"></script> -->
