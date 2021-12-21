5feeeec5b38077e4481fa94d2c8c0b8e12f39ac9
Michael <michaelarnold615@gmail.com>
2021-12-21 10:04:43 -0800

    Javascript date
    Added date script to fetch the current year and updated automatically in the footer

--- modules/footer.php
+++ modules/footer.php
@@ -1,11 +1,16 @@
 <footer class="py-3">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-        <p class="">Copyright &copy; Wayne E. Vinson 2020</p>
-        <a href="https://www.linkedin.com/in/wayne-e-vinson-70426513" target="_blank"><i class="fa fa-linkedin-square fa-2x ipad footer-icon" aria-hidden="true"></i></a>
-        <a href="https://www.facebook.com/VinGroupCPA" target="_blank"><i class="fa fa-facebook-official fa-2x ipad footer-icon" aria-hidden="true"></i></a>
-      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <p class="">
+                    Copyright &copy; Wayne E. Vinson
+                    <script>
+                        document.write(new Date().getFullYear())
+                    </script>
+                </p>
+                <a href="https://www.linkedin.com/in/wayne-e-vinson-70426513" target="_blank"><i class="fa fa-linkedin-square fa-2x ipad footer-icon" aria-hidden="true"></i></a>
+                <a href="https://www.facebook.com/VinGroupCPA" target="_blank"><i class="fa fa-facebook-official fa-2x ipad footer-icon" aria-hidden="true"></i></a>
+            </div>
+        </div>
     </div>
-  </div>
 </footer>
