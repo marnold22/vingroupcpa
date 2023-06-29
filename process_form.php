8c5a47dfa5501ba100d5f98e32a41185ebb34475
Michael <michaelarnold615@gmail.com>
2023-06-29 10:45:50 -0700

    Changed process_form mailer connection to use SMTP server

--- process_form.php
+++ process_form.php
@@ -92,15 +92,30 @@
             $mail = new PHPMailer(true);
 
             // Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_OFF;                              // Enable verbose debug output
-            $mail->isSMTP();                                                 // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                            // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                        // Enable SMTP authentication
-            $mail->Username   = $_ENV['FROM_EMAIL'];                         // SMTP username
-            $mail->Password   = $_ENV['FROM_EMAIL_PASS'];                    // SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;              // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = 587;                                         // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+            // $mail->SMTPDebug = SMTP::DEBUG_OFF;                              // Enable verbose debug output
+            // $mail->isSMTP();                                                 // Send using SMTP
+            // $mail->Host       = 'smtp.gmail.com';                            // Set the SMTP server to send through
+            // $mail->SMTPAuth   = true;                                        // Enable SMTP authentication
+            // $mail->Username   = $_ENV['FROM_EMAIL'];                         // SMTP username
+            // $mail->Password   = $_ENV['FROM_EMAIL_PASS'];                    // SMTP password
+            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;              // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            // $mail->Port       = 587;                                         // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
+            // SMTP Server settings
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;
+            $mail->isSMTP();
+            $mail->Host       = $_ENV["SMTP_HOST"];
+            $mail->SMTPAuth   = true;
+            $mail->Username   = $_ENV["FROM_EMAIL"];
+            $mail->Password   = $_ENV["FROM_EMAIL_PASS"];
+            $mail->Port       = 587;
+            $mail->SMTPOptions = array(
+                'ssl' => array(
+                    'verify_peer' => false,
+                    'verify_peer_name' => false,
+                    'allow_self_signed' => true
+                )
+            );
 
             // Test Settings
             // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
